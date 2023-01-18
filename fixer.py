41e6e8cb1926d3b759f15e5db20cda37f0738a02
Ventsislav Dinev <vdinev94@hotmail.com>
2023-01-18 17:21:59 +0200

    Update fixer.py

--- fixer.py
+++ fixer.py
@@ -4,6 +4,6 @@ import shlex
 
 result = subprocess.run(shlex.split('journalctl -u systemd-udevd --since 1min ago'))
 raw_output = result.stdout
-output = result(raw_output, 'UTF-8')
+output = unicode(raw_output, 'UTF-8')
 if 'ERROR' in output:
     os.system('systemctl restart nodered')
