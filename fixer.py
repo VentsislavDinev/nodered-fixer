be951e60244ac258f3c693d94ad9956991cc25c0
Ventsislav Dinev <vdinev94@hotmail.com>
2023-01-18 17:21:36 +0200

    Update fixer.py

--- fixer.py
+++ fixer.py
@@ -1,6 +1,9 @@
 import os
+import subprocess
+import shlex
 
-stat = os.system('systemctl show -p ActiveState --value nodered ')
-
-if (stat != "active"):
-    os.system('service restart nodered')
+result = subprocess.run(shlex.split('journalctl -u systemd-udevd --since 1min ago'))
+raw_output = result.stdout
+output = result(raw_output, 'UTF-8')
+if 'ERROR' in output:
+    os.system('systemctl restart nodered')
