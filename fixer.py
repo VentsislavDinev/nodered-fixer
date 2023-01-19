f2933615bff38903850ca7d0767e41cd5914f794
Ventsislav Dinev <vdinev94@hotmail.com>
2023-01-19 09:02:25 +0200

    Update fixer.py

--- fixer.py
+++ fixer.py
@@ -8,5 +8,5 @@ import shlex
 #if 'ERROR' in output:
 #    os.system('systemctl restart nodered')
     
-response = subprocess.Popen(["journalctl", "--since", "60 seconds ago", "--no-pager"], stdout=subprocess.PIPE).stdout.read()
+response = subprocess.Popen(["journalctl", "-u nodered", "--since", "60 seconds ago", "--no-pager"], stdout=subprocess.PIPE).stdout.read()
 print(response)
