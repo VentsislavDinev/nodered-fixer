dfca61441d51763d29aebaed51560753bf0fa672
Ventsislav Dinev <vdinev94@hotmail.com>
2023-01-19 15:34:15 +0200

    Update fixer.py

--- fixer.py
+++ fixer.py
@@ -1,16 +1,14 @@
 import os
 import subprocess
 import shlex
+p = subprocess.run('systemctl status nodered | grep error', shell=True, capture_output=True).stdout
+result = str(p, 'utf-8')
+if "error" in result:
+        os.system('systemctl restart nodered')
 
-#result = subprocess.run(shlex.split('journalctl -u systemd-udevd --since 1min ago'))
-#raw_output = result.stdout
-#output = unicode(raw_output, 'UTF-8')
-#if 'ERROR' in output:
-#    os.system('systemctl restart nodered')
-    
-#response = subprocess.Popen(["journalctl", "-u nodered", "--since", "60 seconds ago", "--no-pager"], stdout=subprocess.PIPE).stdout.read()
-#print(response)
-response = subprocess.Popen(["systemctl", " status node-red"], stdout=subproces.PIPE).stdout.read()
-print("response return: ")
-print(response) 
+
+
+
+
+
 
