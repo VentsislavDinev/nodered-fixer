de11e73e0bf783aaedf91a03a958a61473f52b7e
Ventsislav Dinev <vdinev94@hotmail.com>
2023-02-01 08:51:16 +0200

    Add log in fixer.py

--- fixer.py
+++ fixer.py
@@ -1,14 +1,13 @@
 import os
 import subprocess
+from datetime import datetime
 import shlex
+now = datetime.now() # current date and time
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 p = subprocess.run('systemctl status nodered | grep error', shell=True, capture_output=True).stdout
 result = str(p, 'utf-8')
-if "error" in result:
+if "error" in result: 
+        f = open("logger.txt", "a")
+        f.write(date_time,"We restart nodered!")
+        f.close()
         os.system('systemctl restart nodered')
-
-
-
-
-
-
-
